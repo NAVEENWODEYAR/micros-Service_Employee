7c6b7ba714dcf31a0a12dea54ca4f549bef7410a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-16 13:34:00 +0530

    import lombok

--- src/main/java/com/micros/employee/modal/Employee.java
+++ src/main/java/com/micros/employee/modal/Employee.java
@@ -1,4 +1,17 @@
 package com.micros.employee.modal;
 
-public record Employee( Long eId, Long dId, String eName, int eAge, String position) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//public record Employee(Long eId, Long dId, String eName, int eAge, String position) {}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee{
+    private Long eId;
+    private Long dId;
+    private String eName;
+    private int eAge;
+    private String position;
 }
